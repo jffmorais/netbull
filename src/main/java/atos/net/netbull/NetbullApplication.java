c02b071d8ece8065537face699e375536c6e3b0b
Jefferson <jffmorais@outlook.com>
2021-12-11 14:40:55 -0300

    primeiro commit

---
+++ src/main/java/atos/net/netbull/NetbullApplication.java
@@ -0,0 +1,13 @@
+package atos.net.netbull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetbullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetbullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atos/net/netbull/NetbullApplicationTests.java
@@ -0,0 +1,13 @@
+package atos.net.netbull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetbullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
