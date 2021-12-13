8a988ef4239ae36f120ed4fe4e4cc1d79f5c935f
Cauê Ferreira <kau3hc@hotmail.com>
2021-12-13 15:45:54 -0300

    mudança de branch

---
+++ src/test/java/atos/net/netbull/teste/Factory.java
@@ -0,0 +1,13 @@
+package atos.net.netbull.teste;
+
+import atos.net.netbull.repository.entity.ClientePessoaFisicaEntity;
+
+public class Factory {
+	
+	
+	public static ClientePessoaFisicaEntity createClientPF() {
+		
+		ClientePessoaFisicaEntity clientePF = new ClientePessoaFisicaEntity();
+		
+	}
+}
